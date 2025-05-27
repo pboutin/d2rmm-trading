fa424e2415a6ea45f4fae2b2631267ceb843863d
Pascal Boutin <contact@pboutin.net>
2025-05-26 22:58:25 -0400

    POC skillers GC

--- mod/mod.ts
+++ mod/mod.ts
@@ -1,5 +1,13 @@
 const cubemainFilename = "global\\excel\\cubemain.txt";
 const cubemain = D2RMM.readTsv(cubemainFilename);
+const uniqueitemsFilename = 'global\\excel\\uniqueitems.txt';
+const uniqueitems = D2RMM.readTsv(uniqueitemsFilename);
+const itemsFilename = 'hd\\items\\items.json';
+const items = D2RMM.readJson(itemsFilename);
+const itemNamesFilename = 'local\\lng\\strings\\item-names.json';
+const itemNames = D2RMM.readJson(itemNamesFilename);
+const miscFilename = 'global\\excel\\misc.txt';
+const misc = D2RMM.readTsv(miscFilename);
 
 import ITEMS from "./items";
 import RUNES from "./runes";
@@ -10,6 +18,9 @@ import { SELLING_RATE, runesToCodes, valueToRunes } from "./shared";
 const TP_SCROLL = "tsc";
 const MANA_POT = "mpot";
 const HEALTH_POT = "hpot";
+const BASE_SKILLER_ITEM = uniqueitems.rows.find((row) => row.index === "Gheed's Fortune");
+
+let nextUniqueItemId = Math.max(...uniqueitems.rows.map((row) => parseInt(row['*ID'], 10))) + 1;
 
 function adjustDuplicateCodes(codes: string[]) {
   const codeCountMap: Map<string, number> = codes.reduce(
@@ -23,6 +34,80 @@ function adjustDuplicateCodes(codes: string[]) {
   return Array.from(codeCountMap.entries()).map(([code, count]) => {
     if (count === 1) return code;
     return `"${code},qty=${count}"`;
+  });
+}
+
+items.push({
+	cm9: { asset: 'charm/charm_large' },
+});
+
+function declareSkiller(name: string, skillCode: string) {
+  const itemCode = 'cm9';
+
+  misc.rows.push({
+    ...misc.rows.find((row) => row.name === 'Grand Charm'),
+    name: name,
+    level: '50',
+    code: itemCode,
+    unique: '1',
+    // cost: '0',
+    // 'gamble cost': '0',
+    // GheedMin: '1',
+    // GheedMax: '1',
+    // GheedMagicMin: '1',
+    // GheedMagicMax: '1',
+    // GheedMagicLevel: '255',
+    // PermStoreItem: '1',
+    // multibuy: '1',
+    NightmareUpgrade: 'xxx',
+    HellUpgrade: 'xxx',
+  });
+
+  uniqueitems.rows.push({
+    ...BASE_SKILLER_ITEM,
+    index: name,
+    '*ID': `${nextUniqueItemId++}`,
+    lvl: '1',
+    'lvl req': '50',
+    code: itemCode,
+    'cost mult': '1',
+    'cost add': '1',
+    nolimit: '1',
+    carry1: "",
+    prop1: 'skilltab',
+    par1: skillCode,
+    min1: '1',
+    max1: '1',
+    prop2: null,
+    par2: null,
+    min2: null,
+    max2: null,
+    prop3: null,
+    par3: null,
+    min3: null,
+    max3: null,
+    prop4: null,
+    par4: null,
+    min4: null,
+    max4: null,
+  });
+  
+  itemNames.push({
+    id: D2RMM.getNextStringID(),
+    Key: name,
+    enUS: name,
+    zhTW: name,
+    deDE: name,
+    esES: name,
+    frFR: name,
+    itIT: name,
+    koKR: name,
+    plPL: name,
+    esMX: name,
+    jaJP: name,
+    ptBR: name,
+    ruRU: name,
+    zhCN: name,
   });
 }
 
@@ -110,23 +195,32 @@ FACETS.forEach((facet) => {
     `Buying ${facet.name} (Level up)`
   );
 
-  declareRecipe(
-    [facet.deathCode, TP_SCROLL],
-    [...runesToCodes(valueToRunes(facet.value * SELLING_RATE)), TP_SCROLL],
-    `Selling ${facet.name} (Death)`
-  );
-  declareRecipe(
-    [facet.levelCode, TP_SCROLL],
-    [...runesToCodes(valueToRunes(facet.value * SELLING_RATE)), TP_SCROLL],
-    `Selling ${facet.name} (Level up)`
-  );
+  // declareRecipe(
+  //   [facet.deathCode, TP_SCROLL],
+  //   [...runesToCodes(valueToRunes(facet.value * SELLING_RATE)), TP_SCROLL],
+  //   `Selling ${facet.name} (Death)`
+  // );
+  // declareRecipe(
+  //   [facet.levelCode, TP_SCROLL],
+  //   [...runesToCodes(valueToRunes(facet.value * SELLING_RATE)), TP_SCROLL],
+  //   `Selling ${facet.name} (Level up)`
+  // );
 });
 
 // Test skillers
-declareRecipe(["isc", TP_SCROLL], ["cm3,mag"], "Test skillers", {
-  "mod 1": "skilltab",
-  "mod 1 min": "2",
-  "mod 1 max": "2",
-});
+const skillerName = "Bought Harpoonist's";
+declareSkiller(skillerName, "2");
 
+declareRecipe(["isc", TP_SCROLL, HEALTH_POT], [skillerName, "isc", TP_SCROLL], `Buying ${skillerName}`);
+
+const skillerName2 = "Bought Chilling";
+declareSkiller(skillerName2, "5");
+
+declareRecipe(["isc", TP_SCROLL, MANA_POT], [skillerName2, "isc", TP_SCROLL], `Buying ${skillerName2}`);
+
+
+D2RMM.writeTsv(miscFilename, misc);
 D2RMM.writeTsv(cubemainFilename, cubemain);
+D2RMM.writeTsv(uniqueitemsFilename, uniqueitems);
+D2RMM.writeJson(itemsFilename, items);
+D2RMM.writeJson(itemNamesFilename, itemNames);
